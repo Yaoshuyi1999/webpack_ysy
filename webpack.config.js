996e18cc89fb8da5f27ce9fae8055a69449ff348
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-06-27 19:47:55 +0800

    解决了mode警告以及自动更新打包

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
     output: { 
         path: path.resolve(__dirname, "dist"), // 出口路径 绝对路径
         filename: "bundle.js" // 出口文件名
+    },
+    mode: 'development',
+    devServer: {
+        port: 3000, // 端口号
+        open: true
     }
 }
